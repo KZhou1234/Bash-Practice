db323a0c55bcf873b2df2529187c316292ac4323
Ubuntu <ubuntu@ip-172-31-35-109.ec2.internal>
2024-08-14 22:20:30 +0000

    Permission_Check

--- Directory_Check.sh
+++ Directory_Check.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #Write a script that checks if a directory named backup exists in the current directory. If it does not exist, create it.
 #1. check if the directory exist
-back_up_exist=$(ls | grep "backup" | awk '{print $1}')
-
-if [ "$back_up_exist" = "backup" ]
+#back_up_exist=$(ls | grep "backup" | awk '{print $1}')
+if [ "$back_up_exist"=="backup" ]
 then
   echo "backup directory exists"
 else
---
+++ Permission_Check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Write a script to check if a file is readable, writable, and executable.
+#get the file
+echo "Please enter the absolute path to the file"
+read file_path
+ls -l "$file_path"
+
+if [[ -r $file_path ]] 
+then
+  echo "The file is readable"
+elif [[ -w $file_path ]]
+then
+  echo "The file is writable"
+elif [[ -x $file_path ]]
+then
+  echo "The file is executable"
+
+fi
